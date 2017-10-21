929cb0d2165dd282481595d425f94bd2164976d8
liulunsheng <619639650@qq.com>
2017-10-21 09:01:58 +0800

    add handler

--- app/src/main/java/com/sinkeriot/surfaceviewcamera/MainActivity.java
+++ app/src/main/java/com/sinkeriot/surfaceviewcamera/MainActivity.java
@@ -3,6 +3,8 @@ package com.sinkeriot.surfaceviewcamera;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -18,12 +20,13 @@ public class MainActivity extends AppCompatActivity {
     private Button recordbutton;
     private Button stopbutton;
     private MediaRecorder mediaRecorder;
-    File videoFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), System.currentTimeMillis()+ ".mp4");
+    File videoFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), System.currentTimeMillis() + ".mp4");
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
@@ -33,9 +36,52 @@ public class MainActivity extends AppCompatActivity {
         surfaceView.getHolder().setKeepScreenOn(true);
         recordbutton = (Button) this.findViewById(R.id.recordbutton);
         stopbutton = (Button) this.findViewById(R.id.stopbutton);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //startRecording();
+
     }
 
 //    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//
+//        if (mediaRecorder != null) {
+//            mediaRecorder.stop();
+//            mediaRecorder.release();
+//            mediaRecorder = null;
+//        }
+//    }
+
+    private Handler handler = new Handler(){
+
+        @Override
+        public void handleMessage (android.os.Message msg) {
+            switch (msg.what){
+                case 1:
+                    try {
+                        Thread.sleep(20000);
+                        stopRecording();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 2:
+                    startRecording();
+
+                    break;
+            }
+        }
+    };
+
+    //    @zz
 //    public boolean onTouchEvent(MotionEvent event) {
 //        if(event.getAction() == MotionEvent.ACTION_DOWN){
 //            layout.setVisibility(ViewGroup.VISIBLE);
@@ -43,10 +89,10 @@ public class MainActivity extends AppCompatActivity {
 //        return super.onTouchEvent(event);
 //    }
 
-    public void record(View v){
+    public void record(View v) {
         switch (v.getId()) {
             case R.id.recordbutton:
-                try{
+                try {
                     mediaRecorder = new MediaRecorder();
                     mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                     mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
@@ -59,7 +105,10 @@ public class MainActivity extends AppCompatActivity {
                     mediaRecorder.setPreviewDisplay(surfaceView.getHolder().getSurface());
                     mediaRecorder.prepare();
                     mediaRecorder.start();
-                }catch (Exception e) {
+                    Message message = new Message();
+                    message.what = 1;
+                    handler.sendMessage(message);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
                 recordbutton.setEnabled(false);
@@ -67,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.stopbutton:
-                if(mediaRecorder!=null){
+                if (mediaRecorder != null) {
                     mediaRecorder.stop();
                     mediaRecorder.release();
                     mediaRecorder = null;
@@ -79,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startRecording() {
-        try{
+        try {
             mediaRecorder = new MediaRecorder();
             mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
@@ -92,16 +141,23 @@ public class MainActivity extends AppCompatActivity {
             mediaRecorder.setPreviewDisplay(surfaceView.getHolder().getSurface());
             mediaRecorder.prepare();
             mediaRecorder.start();
-        }catch (Exception e) {
+            Message message = new Message();
+            message.what = 1;
+            handler.sendMessage(message);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void stopRecording(){
-        if(mediaRecorder!=null){
+    private void stopRecording() {
+        if (mediaRecorder != null) {
             mediaRecorder.stop();
             mediaRecorder.release();
             mediaRecorder = null;
+
+            Message message = new Message();
+            message.what = 2;
+            handler.sendMessage(message);
         }
     }
 }
